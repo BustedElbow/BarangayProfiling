141f19184ef3baa01dda590dbc60f337934f5761
Miguel <maevenr81@gmail.com>
2024-09-11 21:46:53 +0800

    added Barangay Clearance Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         @vite('resources/css/app.css')
   </head>
 
-   <body>
+   <body class="overflow-y-auto">
      
      <nav class="bg-black flex justify-between p-8">
        <img src="{{ asset('../images/1.png') }}" alt="Placeholder Image" class="ml-64 p-2 w-32 h-32">
@@ -51,21 +51,59 @@
           </div>
           </div>
             <div class=" col-start-7 col-span-6 bg-slate-500 rounded-2xl bg-cover bg-center h-96 bg-hero-pattern"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- News Section -->
+    <div class="container mx-auto mt-24">
+      <div class="grid grid-cols-12 gap-5">
+        <div class="col-span-12 flex justify-between">
+          <h1 class="text-start text-2xl font-bold">News</h1>
+          <button><h3>View More</h3></button>
+        </div>
+        <div class="col-span-3 flex flex-col items-center border p-8">
+          <img src="{{ asset('../images/1.png') }}" alt="asd" class="w-full h-auto">
+          <h2 class="text-xl font-semibold mt-2">Lorem Ipsum Title</h2>
+          <p>September 9, 2024</p>
+        </div>
+        <div class="col-span-3 flex flex-col items-center border p-8">
+          <img src="{{ asset('../images/1.png') }}" alt="asd" class="w-full h-auto">
+          <h2 class="text-xl font-semibold mt-2">Lorem Ipsum Title</h2>
+          <p>September 9, 2024</p>
+        </div>
+        <div class="col-span-3 flex flex-col items-center border p-8">
+          <img src="{{ asset('../images/1.png') }}" alt="asd" class="w-full h-auto">
+          <h2 class="text-xl font-semibold mt-2">Lorem Ipsum Title</h2>
+          <p>September 9, 2024</p>
+        </div>
+        <div class="col-span-3 flex flex-col items-center border p-8">
+          <img src="{{ asset('../images/1.png') }}" alt="asd" class="w-full h-auto">
+          <h2 class="text-xl font-semibold mt-2">Lorem Ipsum Title</h2>
+          <p>September 9, 2024</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Barangay Clearance Section -->
+      <div class="bg-black items-center">
+        <div class="container mx-auto mt-24 ">
+          <div class="grid grid-cols-12 gap-5">
+            <div class="col-span-4 flex flex-col items-center p-8">
+              <img src="{{ asset('../images/1.png') }}" alt="asd" class="w-full h-auto">
+            </div>
+            <div class="col-span-6 flex flex-col items-start justify-center ml-32">
+              <h1 class="text-white text-2xl font-bold">Barangay Clearance</h1>
+              <p class="text-white mt-6 justify-text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos commodi ea, sit ab culpa itaque et in possimus laborum fugiat vitae dicta! Corrupti libero officiis animi sit tempore nulla recusandae.</p>
+              <button class="px-4 py-3 bg-transparent border rounded-button text-white uppercase mt-6">Learn More</button>
+            </div>
           </div>
+        </div>
       </div>
-    </div>
 
-    <div class="ml-64 mt-32 flex justify-between">
-      <div class="flex-1">
-        <h1 class="mt-11 text-2xl uppercase font-bold">Be an SK NOW!</h1>
-        <p class="mt-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus corporis officia aliquam perspiciatis minima sed minus suscipit porro ex assumenda, eaque optio eius, reprehenderit consequatur quibusdam voluptatibus, doloremque qui similique.</p>
-        <button class="mt-6 px-4 py-3 bg-black border rounded-button text-white uppercase"> Register</button>
-      </div>
-      <div class="flex-1 flex  ml-32">
-        <img src="{{ asset('../images/2.png') }}" alt="Placeholder Image" class="w-554 h-326">
-      </div>
-    </div>
-
-
+      <!-- Barangay Officials Section -->
+       <div class="container mx-auto">
+        <h1>Barangay Officials</h1>
+       </div>
   </body>
 </html>
